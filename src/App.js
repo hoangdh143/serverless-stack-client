8f6b52a312b4c923247041a38abf60eafd9c8b8e
HoangDH <huuhoang.dao@gmail.com>
2019-12-11 15:36:51 +0700

    Update Login Redirect

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React, { useState, useEffect } from "react";
-import {Link, withRouter} from "react-router-dom";
-import {Nav, Navbar, NavItem} from "react-bootstrap";
-import {LinkContainer} from "react-router-bootstrap";
+import { Auth } from "aws-amplify";
+import { Link, withRouter } from "react-router-dom";
+import { Nav, Navbar, NavItem } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 import Routes from "./Routes";
 import "./App.css";
-import {Auth} from "aws-amplify";
 
 function App(props) {
-
-    const [isAuthenticated, userHasAuthenticated] = useState(false);
     const [isAuthenticating, setIsAuthenticating] = useState(true);
-
-    async function handleLogout() {
-        await Auth.signOut();
-        userHasAuthenticated(false);
-        props.history.push("/login");
-    }
+    const [isAuthenticated, userHasAuthenticated] = useState(false);
 
     useEffect(() => {
         onLoad();
@@ -25,39 +18,59 @@ function App(props) {
         try {
             await Auth.currentSession();
             userHasAuthenticated(true);
-        } catch (e) {
+        }
+        catch(e) {
             if (e !== 'No current user') {
                 alert(e);
             }
         }
+
         setIsAuthenticating(false);
     }
 
+    async function handleLogout() {
+        await Auth.signOut();
+
+        userHasAuthenticated(false);
+
+        props.history.push("/login");
+    }
+
     return (
-        !isAuthenticating &&
-        <div className="App container">
-            <Navbar fluid collapseOnSelect>
-                <Navbar.Header>
-                    <Navbar.Brand>
-                        <Link to="/">Scratch</Link>
-                    </Navbar.Brand>
-                    <Navbar.Toggle/>
-                </Navbar.Header>
-                <Navbar.Collapse>
-                    <Nav pullRight>
-                        {isAuthenticated ? <NavItem onClick={handleLogout}>Logout</NavItem> : <>
-                            <LinkContainer to="/signup">
-                                <NavItem>Signup</NavItem>
-                            </LinkContainer>
-                            <LinkContainer to="/login">
-                                <NavItem>Login</NavItem>
-                            </LinkContainer>
-                        </>}
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-            <Routes appProps={{isAuthenticated, userHasAuthenticated}}/>
-        </div>
+        !isAuthenticating && (
+            <div className="App container">
+                <Navbar fluid collapseOnSelect>
+                    <Navbar.Header>
+                        <Navbar.Brand>
+                            <Link to="/">Scratch</Link>
+                        </Navbar.Brand>
+                        <Navbar.Toggle />
+                    </Navbar.Header>
+                    <Navbar.Collapse>
+                        <Nav pullRight>
+                            {isAuthenticated ? (
+                                <>
+                                    <LinkContainer to="/settings">
+                                        <NavItem>Settings</NavItem>
+                                    </LinkContainer>
+                                    <NavItem onClick={handleLogout}>Logout</NavItem>
+                                </>
+                            ) : (
+                                <>
+                                    <LinkContainer to="/signup">
+                                        <NavItem>Signup</NavItem>
+                                    </LinkContainer>
+                                    <LinkContainer to="/login">
+                                        <NavItem>Login</NavItem>
+                                    </LinkContainer>
+                                </>
+                            )}
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+                <Routes appProps={{ isAuthenticated, userHasAuthenticated }} />
+            </div>
+        )
     );
 }
 
--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export default {
   MAX_ATTACHMENT_SIZE: 5000000,
+  STRIPE_KEY: "pk_test_t9u5mqNyejuKVq9As4vIReji00Fk2Ofi8H",
   s3: {
     REGION: "ap-southeast-1",
     BUCKET: "notes-app-hoangdh"
